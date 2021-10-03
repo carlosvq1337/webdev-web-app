c5561cf71c7a6b07bed014d426f95b32c1876501
Carlos Vargas <carlosvq1337@gmail.com>
2021-10-03 15:15:38 -0600

    Navbar changes, WIP to make it work on mobile

--- client/src/components/Loginbar.jsx
+++ client/src/components/Loginbar.jsx
@@ -1,33 +1,133 @@
 
-import React, { Component } from 'react'
-import { Link } from 'react-router-dom'
+import React from 'react'
+import { NavLink as Link} from 'react-router-dom'
 import styled from 'styled-components'
-import Logo from './Logo'
-import NavButtons from './NavButtons'
+import {FaBars} from 'react-icons/fa'
 
-
-const Wrapper = styled.div.attrs({
-    className: 'form-group',
-})`
-    margin-bottom: 75px;
-    margin-left: 40px;
-    padding-top: 40px;
+//STYLING
+const Nav = styled.nav`
+background: #000;
+height: 80px;
+display: flex;
+justify-content: space-between;
+padding: 0.5rem calc((100vw - 1000px)/2);
+z-idex: 10;
 `
 
-class Loginbar extends Component {
-    render() {
-        return (
+const NavLink = styled(Link)`
+color: #fff;
+display: flex;
+align-items: center;
+text-decoration: none;
+padding: 0 1rem;
+height: 100%;
+cursor: pointer;
+&.active {
+    color: #15cdfc;
+}
+`
 
-            <Wrapper>
- 
-                    <Logo />
-                    <Link to="/Login" style={{color: "#150050", textDecoration: 'none'}}>Iniciar sesión / Registrarse</Link>
-                    <NavButtons />
+const Bars = styled(FaBars)`
+display: none;
+color: #fff;
+@media screen and (max-width: 768px){
+    display: block;
+    position: absolute;
+    top: 0;
+    right: 0;
+    transform: translate(-100%, 75%);
+    font0size: 1.8rem;
+    cursor: pointer;
+}
+`
 
-            </Wrapper>
+const NavMenu = styled.div`
+display: flex;
+align-items: center;
+margin-right: --24px;
 
-        )
-    }
+@media sceen and (max-width: 768px){
+    display: none;
+}
+`
+
+const NavBtn = styled.nav`
+display: flex;
+align-items: center;
+margin-right: 24px;
+
+@media screen and (max-width: 768px){
+    display: none;
+}
+`
+const NavBtnLink = styled(Link)`
+border-radius: 4px;
+background: #256ce1;
+padding: 10px 22px;
+color #fff;
+border: none;
+outline: none;
+cursor: pointer;
+transition: all 0.2s ease-in-out;
+text-decoration: none;
+
+&:hover{
+    transition: all 0.2s ease-in-out;
+    background: #fff;
+    color: #010606;
+}
+`
+
+
+const Loginbar = () => {
+    return (
+        <>
+            <Nav>
+                <NavLink to="/Homepage">
+                    Home
+                </NavLink>
+                <Bars/>
+                <NavMenu>
+                    <NavLink to="/contents" activeStyle>
+                        Contents
+                    </NavLink>           
+                </NavMenu>
+                <NavBtn>
+                    <NavBtnLink to="/signin">
+                        Sign In
+                    </NavBtnLink>
+                </NavBtn>
+            </Nav>
+        </>
+    )
 }
 
 export default Loginbar
+
+
+
+// const Wrapper = styled.div.attrs({
+//     className: 'form-group',
+// })`
+//     margin-bottom: 75px;
+//     margin-left: 40px;
+//     padding-top: 40px;
+// `
+
+// class Loginbar extends Component {
+//     render() {
+//         return (
+
+//             <Wrapper>
+ 
+//                     <Logo />
+//                     <Link to="/Login" style={{color: "#150050", textDecoration: 'none'}}>Iniciar sesión / Registrarse</Link>
+//                     <NavButtons />
+
+//             </Wrapper>
+
+//         )
+//     }
+// }
+
+// export default Loginbar
